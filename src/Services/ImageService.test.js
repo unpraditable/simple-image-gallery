2c5b488a5cb8fbc163f1685ba87fc71588736b34
Aditya Pradita <pradita397@gmail.com>
2021-12-18 01:21:42 +0700

    Add ImageService.test

---
+++ src/Services/ImageService.test.js
@@ -0,0 +1,26 @@
+import axios from "axios";
+import { APP_KEY } from "../Constants/constants";
+import ImageService from "./ImageService";
+
+describe("ImageService", () => {
+  describe("getImages", () => {
+    it("calls axios API with correct params", () => {
+      //given
+      jest.spyOn(axios, "get");
+
+      //when
+      ImageService.getImages();
+
+      //then
+      expect(axios.get).toHaveBeenCalledWith(
+        "https://api.unsplash.com/photos",
+        {
+          params: {
+            client_id: APP_KEY,
+            per_page: 24,
+          },
+        }
+      );
+    });
+  });
+});
